d419e0a5d9b9c31a16c07fd10b05b12c12f2d781
Amiruriza <getvpstandvp@gmail.com>
2021-03-21 14:16:09 +0700

    aws db integration

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const { User } = require('./models')
 
-app.get('/', async (req, res) => {
-  
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/users', async (req, res) => {
   try {
     const users = await User.findAll()
     res.send(users)
